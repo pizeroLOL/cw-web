7d818d57aac283c8f77b613c596dd722290a4a16
Pizero <71167373+pizeroLOL@users.noreply.github.com>
2025-01-25 03:16:13 +0800

    fix(meme): 顶部错误的 padding

--- src/components/meme/box.tsx
+++ src/components/meme/box.tsx
@@ -8,7 +8,7 @@ export interface BoxProps {
 }
 
 export default component$<{ it: BoxProps }>(({ it }) => (
-  <section class="bg-gary-100 my-4 flex break-inside-avoid-column flex-col flex-nowrap gap-2 rounded-md py-4 *:w-full first:mt-0 dark:bg-gray-900">
+  <section class="bg-gary-100 my-4 flex break-inside-avoid-column flex-col flex-nowrap gap-2 rounded-md pb-4 *:w-full first:mt-0 dark:bg-gray-900">
     {/*eslint-disable-next-line qwik/jsx-img*/}
     <img src={it.data} width={256} class="rounded-t-md" />
     <div class="px-4 text-xl">{it.title}</div>
